094df4e7b4774f3a884691132498c4817bfb66c5
PauloAlves8039 <pj38alves@gmail.com>
2025-04-10 15:04:04 -0300

    feat: add class for validation

---
+++ src/QrCode.Api/Validation/ModelExceptionValidation.cs
@@ -0,0 +1,14 @@
+namespace QrCode.Api.Validation;
+
+public class ModelExceptionValidation : Exception
+{
+    public ModelExceptionValidation(string error) : base(error) { }
+
+    public static void When(bool hasErros, string error)
+    {
+        if (hasErros)
+        {
+            throw new ModelExceptionValidation(error);
+        }
+    }
+}
